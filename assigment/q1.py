82b1b7e821467ddb740f10ec485e1f17c131058c
TIKIYEON <tarikyorulmaz818@gmail.com>
2024-09-10 15:02:40 +0200

    SIR actually works

--- assigment/q1.py
+++ assigment/q1.py
@@ -1,22 +1,24 @@
 import random
 import scipy as sp
 
-qx = random.uniform(0, 15)
-
 
 def p(x):
-    0.3+sp.stats.uniform.pdf(x, 2.0, 1.0)
-    + 0.4+sp.stats.uniform.pdf(x, 5.0, 2.0)
-    + 0.3+sp.stats.uniform.pdf(x, 9.0, 1.0)
+    return 0.3+sp.stats.norm.pdf(x, 2.0, 1.0)
+    + 0.4+sp.stats.norm.pdf(x, 5.0, 2.0)
+    + 0.3+sp.stats.norm.pdf(x, 9.0, 1.0)
 
 def sir(k):
-    samples = [qx for i in range(k)]
+    samples = [random.uniform(0, 15) for _ in range(k)]
 
     weights = [p(x) for x in samples]
     sumWeights = sum(weights)
     normWeights = [w / sumWeights for w in weights]
 
-    reSampling = random.choices(samples, normWeights, k)
+    reSampling = random.choices(samples, weights=normWeights, k=k)
 
     return reSampling
 
+k20 = sir(20)
+k100 = sir(100)
+k1000 = sir(1000)
+print(k20)
